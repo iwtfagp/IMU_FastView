741d0a15ee23fe72d464511720bc3ef7ef7d5004
William@lab <iwtfagp@gmail.com>
2015-03-01 16:17:32 +0800

    New File IMU_FastToImage

--- IMU_FastToImage.m
+++ IMU_FastToImage.m
@@ -1,10 +1,5 @@
 close all;clear all;clc;
-
-[FileName, PathName, FilterIndex] = uigetfile('.txt');
-PathName = [PathName FileName];
-
-data=load(PathName);
-
+list = dir('*.txt');
 header={'Left Hip','Right Hip','Left Knee','Right Knee',    ...
 'Back Roll','Back yaw','Back Pitch',                        ...
 'Left Thigh Roll','Left Thigh yaw','Left Thigh Pitch',      ...
@@ -12,12 +7,17 @@ header={'Left Hip','Right Hip','Left Knee','Right Knee',    ...
 'Left Shank Roll','Left Shank yaw','Left Shank Pitch',      ...
 'Right Shank Roll','Right Shank yaw','Right Shank Pitch'}
 
-for i = 1:4
-    set(gcf,'numbertitle','off')      
-    set(gcf,'name',FileName)
-    subplot(2,2, i)
-    plot(data(:,i))
-    title(header(i))
-end
-% saveas(gcf,[strtok(FileName, '.'),'.jpg']);
+for fileNum = 1:size(list)
+    FileName = list(fileNum).name;
+    data=load(FileName);
 
+    for i = 1:4
+        set(gcf,'numbertitle','off')      
+        set(gcf,'name',FileName)
+        subplot(2,2, i)
+        plot(data(:,i))
+        title(header(i))
+    end
+    saveas(gcf,[strtok(FileName, '.'),'.jpg']);
+end
+close all;
