72377436e76c01571c79d6d48f9382070ed99ee8
William@lab <iwtfagp@gmail.com>
2015-03-17 14:09:27 +0800

    You can choose new dir position

--- IMU_FastToImage.m
+++ IMU_FastToImage.m
@@ -1,15 +1,20 @@
 close all;clear all;clc;
-list = dir('*.txt');
+
+
+filedir = uigetdir
+% fullfile(matlabroot, filedir);
+list = dir([ filedir,'\*.txt'])
 header={'Left Hip','Right Hip','Left Knee','Right Knee',    ...
 'Back Roll','Back yaw','Back Pitch',                        ...
 'Left Thigh Roll','Left Thigh yaw','Left Thigh Pitch',      ...
 'Right Thigh Roll','Right Thigh yaw','Right Thigh Pitch',   ...
 'Left Shank Roll','Left Shank yaw','Left Shank Pitch',      ...
-'Right Shank Roll','Right Shank yaw','Right Shank Pitch'}
+'Right Shank Roll','Right Shank yaw','Right Shank Pitch'};
 
 for fileNum = 1:size(list)
     FileName = list(fileNum).name;
-    data=load(FileName);
+    NewFilename = [filedir, '\', FileName]
+    data=load(NewFilename);
 
     for i = 1:4
         set(gcf,'numbertitle','off')      
@@ -18,6 +23,6 @@ for fileNum = 1:size(list)
         plot(data(:,i))
         title(header(i))
     end
-    saveas(gcf,[strtok(FileName, '.'),'.jpg']);
+    saveas(gcf,[strtok(NewFilename, '.'),'.jpg']);
 end
 close all;
